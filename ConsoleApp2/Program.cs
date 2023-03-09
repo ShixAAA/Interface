9e4df9ddbbca33649cb080568d95c04e403db916
user11 <user11@test.ru>
2023-03-09 15:38:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Билет на междугородный транспорт.";
+            Ticket ticket1 = new Ticket("23131", "Липецк", 12,"13:34");
+            Ticket ticket2 = new Ticket("12372", "Казань", 2, "00:04");
+            Ticket ticket3 = new Ticket("7767", "Владивосток", 25, "07:14");
+            Ticket[] ticketLot = new Ticket[] { ticket1, ticket2, ticket3 };
+            Array.Sort(ticketLot);
+            Console.WriteLine("Рейсы, отсортированные по месту:\n");
+            foreach (Ticket car in ticketLot)
+                Console.WriteLine(car.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Ticket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Ticket: IComparable
+    {
+        public string Flight { get; set; }
+        public string Destination { get; set; }
+        public int Number { get; set; }
+        public string Time { get; set; }
+
+        public Ticket(string flight, string destination, int number, string time)
+        {
+            Flight = flight;
+            Destination = destination;
+            Number = number;
+            Time = time;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Данные о рейсе:\n" + "- Рейс: {0}\n" + "- Пункт назначения: {1}\n" + "- Время отправления: {2}\n"+ "- Номер места: {3}\n", Flight, Destination, Time, Number);
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            const string s = "Сравниваемый объект не принадлежит классу Ticket";
+            Ticket ticket = obj as Ticket;
+            if (ticket != null) return this.Number.CompareTo(ticket.Number);
+            else throw new ArgumentException(s);
+        }
+    }
+
+}
